cad690cfda04246143e76ad5f64e69278091b287
ahyaemon <iamahyaemon@gmail.com>
2020-10-14 00:26:47 +0900

    terraform fmt

--- terraform/modules/frontend/ssm.tf
+++ terraform/modules/frontend/ssm.tf
@@ -1,11 +1,11 @@
 resource "aws_ssm_parameter" "user_pool_id" {
-  name = "/cognito/USER_POOL_ID"
-  type = "SecureString"
+  name  = "/cognito/USER_POOL_ID"
+  type  = "SecureString"
   value = aws_cognito_user_pool.user_pool.id
 }
 
 resource "aws_ssm_parameter" "user_pools_web_client_id" {
-  name = "/cognito/USER_POOLS_WEB_CLIENT_ID"
-  type = "SecureString"
+  name  = "/cognito/USER_POOLS_WEB_CLIENT_ID"
+  type  = "SecureString"
   value = aws_cognito_user_pool_client.user_pool_client.id
 }
